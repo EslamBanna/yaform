12752297b9ab29bb197586fd3e89e33d0b4f7289
EslamBanna <solombana2000@gmail.com>
2021-12-08 15:41:42 +0000

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function login(){
+
+    }
+    public function logout(){
+
+    }
+    public function signup(){
+
+    }
+    public function updateInfo(){
+
+    }
+    public function getUserInfo(){
+        
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::group(['prefix' => 'unauth'], function(){
+Route::post('/login', [UserController::class,'login']);
+Route::post('/logout', [UserController::class,'logout']);
+Route::post('/sig-up', [UserController::class,'signup']);
+Route::post('/update-info', [UserController::class,'updateInfo']);
+Route::post('/get-info', [UserController::class,'getUserInfo']);
+
+});
